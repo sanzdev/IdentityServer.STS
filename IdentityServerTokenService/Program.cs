fc52460149187490645ae2a4fd1d5dfe7eb80f96
Sanath <schakkarayan@globalblue.com>
2018-08-16 21:02:09 +0800

    Intial commit

---
+++ IdentityServerTokenService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace IdentityServerTokenService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            if (Debugger.IsAttached || args.Contains("--debug"))
+            {
+                host.Run();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", false)
+              .Build();
+
+            var hostOptions = config.GetSection("HostOptions");
+
+            return WebHost.CreateDefaultBuilder(args)
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .UseApplicationInsights()
+              .Build();
+        }
+    }
+}
---
+++ IdentityServerTokenService/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServerTokenService
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+    {
+      Configuration = configuration;
+
+      _hostingEnvironment = hostingEnvironment;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    private readonly IHostingEnvironment _hostingEnvironment;
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    }
+    
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      if (env.IsDevelopment())
+      {
+        loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+        loggerFactory.AddDebug();
+      }
+
+      UseForwardedHeaders(app);
+      app.UseDeveloperExceptionPage();
+    }
+
+    private static void UseForwardedHeaders(IApplicationBuilder app)
+    {
+      var forwardedHeadersOptions = new ForwardedHeadersOptions
+      {
+        RequireHeaderSymmetry = false,
+        ForwardedHeaders = ForwardedHeaders.All,
+      };
+
+      forwardedHeadersOptions.KnownNetworks.Clear();
+      forwardedHeadersOptions.KnownProxies.Clear();
+
+      app.UseForwardedHeaders(forwardedHeadersOptions);
+    }      
+  }
+}
